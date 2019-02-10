3ea17d2dc65dc4990a446a776268bd461408a0ce
Jonathan Hector <appdevelopment87@gmail.com>
2019-02-10 08:07:52 +0100

    First commit of Swift learning

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jonathan Hector on 07.02.19.
+//  Copyright © 2019 Jonathan Hector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
